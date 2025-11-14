ee92543089b8f40db11e9f1ccf67bb496cdd3766
maxcore25 <mkorobov2002@mail.ru>
2025-11-14 10:35:24 +0300

    Enhance user schema with additional fields and introduce tutor schema
    - Added firstName, lastName, middleName, phone, knowledgeLevel, and role fields to the user schema for comprehensive user data representation.
    - Introduced a new tutor schema extending the user schema, including portfolio, rating, and testimonialsCount for enhanced tutor profile management.
    - Updated the user type inference to include the new Tutor type.

--- frontend/src/entities/user/model/user.schema.ts
+++ frontend/src/entities/user/model/user.schema.ts
@@ -1,15 +1,24 @@
+import { levels } from '@/shared/config';
 import { z } from 'zod';
-
-export const RoleSchema = z.object({
-  id: z.uuid(),
-  name: z.string().min(1),
-});
 
 export const userSchema = z.object({
   id: z.uuid(),
+  firstName: z.string().min(1),
+  lastName: z.string().min(1),
+  middleName: z.string().optional(),
   email: z.email(),
-  name: z.string().min(1),
-  roles: z.array(RoleSchema).nonempty(),
+  phone: z.string().optional(),
+  knowledgeLevel: z.enum(levels),
+  role: z.string().min(1),
+  createdAt: z.iso.datetime(),
+  updatedAt: z.iso.datetime(),
+});
+
+export const tutorSchema = userSchema.extend({
+  portfolio: z.string(),
+  rating: z.number().nonnegative(),
+  testimonialsCount: z.number().int().nonnegative(),
 });
 
 export type User = z.infer<typeof userSchema>;
+export type Tutor = z.infer<typeof tutorSchema>;
