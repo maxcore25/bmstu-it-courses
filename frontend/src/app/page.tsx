cac496417a0e2b05ae16efcf3ff894ea2d67bafa
maxcore25 <mkorobov2002@mail.ru>
2025-11-15 07:55:29 +0300

    Update HomePage layout and enhance button styles
    - Revamped the HomePage layout to feature a centered title and description for better visual appeal.
    - Added a CSS rule to ensure buttons have a pointer cursor when not disabled, improving user interaction.

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -5,7 +5,15 @@ export default function HomePage() {
     <>
       <PublicHeader />
       <main className='container mx-auto'>
-        <h1>home page</h1>
+        <div className='grid place-content-center gap-6 text-center'>
+          <h1 className='croll-m-20 mt-8 text-4xl font-extrabold tracking-tight text-balance'>
+            Школа IT-курсов CodeCraft
+          </h1>
+          <p className='text-muted-foreground max-w-xl text-xl'>
+            Современная IT-школа CodeCraft — учитесь у экспертов и стройте
+            карьеру будущего!
+          </p>
+        </div>
       </main>
     </>
   );
