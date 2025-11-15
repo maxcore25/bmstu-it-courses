e1b6bcf82099389b7630bed69c738035f5d11834
maxcore25 <mkorobov2002@mail.ru>
2025-11-15 11:02:58 +0300

    Refactor TutorsSection component to enhance layout and user interface
    - Updated the TutorsSection component to include a user icon from lucide-react for better visual representation.
    - Adjusted the CardHeader layout to improve spacing and alignment, enhancing the overall appearance.
    - Removed the commented-out Image component and replaced it with a placeholder user icon, streamlining the code.

--- frontend/src/_pages/landing/ui/tutors-section/index.tsx
+++ frontend/src/_pages/landing/ui/tutors-section/index.tsx
@@ -1,8 +1,7 @@
 import { getTutorsSSR } from '@/entities/user/api/get-tutors-ssr.api';
 import { Badge } from '@/shared/ui/badge';
 import { Card, CardContent, CardHeader } from '@/shared/ui/card';
-import { Star } from 'lucide-react';
-import Image from 'next/image';
+import { Star, User } from 'lucide-react';
 
 export const TutorsSection = async () => {
   const tutors = await getTutorsSSR();
@@ -25,17 +24,20 @@ export const TutorsSection = async () => {
           {tutors.map((tutor, index) => (
             <Card
               key={index}
-              className='group overflow-hidden py-0 transition-all duration-300 hover:-translate-y-2 hover:shadow-xl'
+              className='group gap-0 overflow-hidden py-0 transition-all duration-300 hover:-translate-y-2 hover:shadow-xl'
             >
-              <CardHeader className='p-0'>
-                <div className='relative aspect-square overflow-hidden'>
-                  <Image
+              <CardHeader className='gap-0 p-0'>
+                <div className='bg-muted relative grid aspect-square place-content-center overflow-hidden'>
+                  {/* <Image
                     src={'/vercel.svg'}
                     alt={`${tutor.firstName} ${tutor.lastName}`}
                     className='h-full w-full object-cover transition-transform duration-500 group-hover:scale-110'
                     sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw'
                     fill
-                  />
+                  /> */}
+                  <span className='grid w-fit place-content-center rounded-full bg-gray-200 p-6'>
+                    <User className='size-16 text-gray-400' />
+                  </span>
                   <div className='absolute inset-0 bg-linear-to-t from-black/60 via-transparent to-transparent opacity-0 transition-opacity duration-300 group-hover:opacity-100' />
                 </div>
               </CardHeader>
