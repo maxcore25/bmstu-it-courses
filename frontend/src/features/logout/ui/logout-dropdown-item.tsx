95713af223760b708c5574dc45fd2b73bd3d6dac
maxcore25 <mkorobov2002@mail.ru>
2025-11-16 22:22:42 +0300

    Implement logout functionality with user state reset
    - Updated LogoutDropdownItem component to clear user state upon successful logout.
    - Integrated useAuthStore to set user to null after logout, enhancing user experience and state management.

--- frontend/src/features/logout/ui/logout-dropdown-item.tsx
+++ frontend/src/features/logout/ui/logout-dropdown-item.tsx
@@ -5,16 +5,19 @@ import { DropdownMenuItem } from '@/shared/ui/dropdown-menu';
 import { LogOut } from 'lucide-react';
 import { useRouter } from 'next/navigation';
 import { useLogout } from '../model/use-logout';
+import { useAuthStore } from '@/features/auth';
 
 export const LogoutDropdownItem = () => {
   const router = useRouter();
   const { mutate } = useLogout();
+  const setUser = useAuthStore(s => s.setUser);
 
   const handleLogout = () => {
     mutate(undefined, {
       onSuccess: () => {
         clearAuthTokens();
         router.push('/login');
+        setUser(null);
       },
     });
   };
